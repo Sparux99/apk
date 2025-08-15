f097da91cea594419b17216b45394552679a508c
sparux99 <maribiamine@gmail.com>
2025-08-15 12:27:11 +0100

    Fix AndroidManifest.xml again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,33 +1,56 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id 'com.android.application'
 }
 
 android {
-    namespace = "com.amine.app"
-    compileSdk = 35
+    namespace 'com.amine.app'
+    compileSdk 35
 
     defaultConfig {
-        applicationId = "com.appliberated.helloworldselfaware"
-        minSdk = 9
-        targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        applicationId "com.amine.app"
+        minSdk 21
+        targetSdk 35
+        versionCode 1
+        versionName "1.0"
+
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+        debug {
+            
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        viewBinding true
     }
 }
 
 dependencies {
+    // Material Design Components
+    implementation 'com.google.android.material:material:1.10.0'
+
+    // AppCompat
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+
+    // Core
+    implementation 'androidx.core:core-ktx:1.12.0'
+
+    // ConstraintLayout
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+
+    // Testing
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
 }
