9c7a6e10959d4dbee28f131e697ecfd49aae0e57
sparux99 <maribiamine@gmail.com>
2025-08-15 15:54:07 +0100

    Upload player app v1.12

--- app/src/main/java/com/amine/player/MainActivity.kt
+++ app/src/main/java/com/amine/player/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
