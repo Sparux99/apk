8638d2d3e36d8de8fe0b4f192de226c2c484d486
sparux99 <maribiamine@gmail.com>
2025-08-17 00:19:02 +0100

    Build App v3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,11 @@ dependencies {
     
     // Google's Media3 (ExoPlayer) for video playback
     implementation("com.github.vkay94:DoubleTapPlayerView:master-SNAPSHOT")
-    implementation("androidx.media3:media3-exoplayer:1.3.1")
-    implementation("androidx.media3:media3-ui:1.3.1")
+
+    //implementation("androidx.media3:media3-exoplayer:1.3.1")
+    //implementation("androidx.media3:media3-ui:1.3.1")
+    implementation("com.google.android.exoplayer:exoplayer:2.20.1")
+    implementation("com.github.vkay94:DoubleTapPlayerView:2.0.0")
 
     // RecyclerView to show video list
     implementation("androidx.recyclerview:recyclerview:1.3.2")
